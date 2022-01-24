97c0b57cea471c00540a982b4d08e6bda47142fa
rawziNael <rawzi.homran@yahoo.com>
2022-01-24 16:14:12 +0200

    Task5

---
+++ app2.js
@@ -0,0 +1,10 @@
+var myArray= [];
+let color = prompt("what is your favorite color?");
+myArray[0]= color;
+let money = prompt("How much money do you have?")
+myArray[1] = money;
+let civilStatus = prompt("Are you single?")
+myArray[2] = civilStatus;
+for(var i =0 ;i<=3; i++){
+    console.log(myArray[i]);
+}
