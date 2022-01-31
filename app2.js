784965595e49fa05a27ab628e527e1272df812d3
rawziNael <rawzi.homran@yahoo.com>
2022-01-31 15:04:31 +0200

    task6

--- app2.js
+++ app2.js
@@ -1,10 +1,53 @@
-var myArray= [];
-let color = prompt("what is your favorite color?");
-myArray[0]= color;
-let money = prompt("How much money do you have?")
-myArray[1] = money;
-let civilStatus = prompt("Are you single?")
-myArray[2] = civilStatus;
-for(var i =0 ;i<=3; i++){
-    console.log(myArray[i]);
+var array = [];
+
+function UserLiving() {
+    var userHome = prompt("Do you live in Jordan?");
+    userHome.toLowerCase();
+    if (userHome === "yes" || userHome === "y") {
+        array.push(userHome);
+    } else if (userHome === "No" || userHome === "n") {
+        array.push(userHome);
+    }
+    else {
+        array.push('invalid');
+    }
+    return array;
 }
+
+
+function UserStudent() {
+    var userStudy = prompt("Are you still studing?");
+    userStudy.toLowerCase();
+    if (userStudy === "yes" || userStudy === "y") {
+        array.push(userStudy);
+    } else if (userStudy === "No" || userStudy === "n") {
+        array.push(userStudy);
+    }
+    else {
+        array.push('Invalid');
+    }
+    return array;
+}
+
+function UserRelation() {
+    var userSingle = prompt("Are you single?");
+    userSingle.toLowerCase();
+    if (userSingle === "yes" || userSingle === "y") {
+        array.push(userSingle);
+    } else if (userSingle === "no" || userSingle === "n") {
+        array.push(userSingle);
+    }
+    else {
+        array.push('Invalid');
+    }
+    return array;
+}
+
+function printArray(arr) {
+    return console.log(arr);
+}
+
+UserLiving()
+UserStudent();
+UserRelation();
+printArray(array);
